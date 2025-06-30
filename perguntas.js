8d538cb0ec620b08e9cb06159820d503786a3678
Nathália Vitória Gomes <vitoria.gomes.nathalia@escola.pr.gov.br>
2025-06-30 10:44:25 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+   'Lingua portuguesa',
+   'Como se diz boneca em Português?',
+   'Boneca em português é boneca'
+  )
+
+criaCartao(
+  'Matematica',
+  'Quanto é 10+10?',
+  '10+10 é igual a 20'
+  )
